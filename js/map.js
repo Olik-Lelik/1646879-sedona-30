1f1c260212e789fbcb2de948a3fb08ba77c0c97e
Olik-Lelik <73963072+Olik-Lelik@users.noreply.github.com>
2021-03-10 02:55:14 +0300

    JS и анимация на главной странице

---
+++ js/map.js
@@ -0,0 +1,6 @@
+const buttonLink = document.querySelector(".button-link");
+const formPopup = document.querySelector(".popup");
+
+buttonLink.addEventListener("click", function() {
+formPopup.classList.toggle('modal-show');
+})
